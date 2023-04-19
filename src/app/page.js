155d65a59b893defb41a94e7fd6cba3bd48a8b82
Kimdonggyu <qweszx33@naver.com>
2023-04-19 21:19:12 +0900

    test: how to use JSX in Next Js create-time:2023-04-19 21:20

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 
 
 export default function Home() {
+
+  const frameWord = "nextJS";
+  const URL = "http://google.com"
+
   return (
     <div>
-      <h4 className="red">Hello Wordl</h4>
+      <h4 className="title">Apple Store</h4>
+      <p className="title-sub">by dev { frameWord }</p>
+      <a href={URL}>Link</a>
     </div>
   )
 }
