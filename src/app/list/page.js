c4c69773b9603ca4e7e2ef199894037207b03382
Kimdonggyu <qweszx33@naver.com>
2023-04-22 00:22:12 +0900

    feat:create mapping modified layout create-time:2023-04-22 00:22

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,18 +1,22 @@
 
 
 export default function ListPage() {
-  const goods1 = 40;
-  const goods2 = 50;
+  const structures = ['Tomato', 'Pasta', 'Coconut'];
+  const costs = [40, 50, 100]
+
   
   return (
     <div>
       <h4 className="title">상품 목록</h4>
-      <div className="food">
-        <h4>상품1 ${goods1}</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 ${goods2}</h4>
-      </div>
+      {
+        structures.map((i,index)=>{
+          return(
+            <div className="food">
+              <h4>{i} is{costs[index]}$</h4>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
