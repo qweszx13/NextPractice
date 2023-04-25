06bd00b63dd0114072882c0f1e98283190ed1c72
Kimdonggyu <qweszx33@naver.com>
2023-04-25 14:23:23 +0900

    test:create useState button create-time:2023-04-25 14:23

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 import food0 from "/public/food0.png";
 import food1 from "/public/food1.png";
 import food2 from "/public/food2.png";
@@ -8,7 +11,7 @@ export default function ListPage() {
   const structures = ['Tomato', 'Pasta', 'Coconut'];
   const costs = [40, 50, 100];
   const images = [food0, food1, food2];
-
+  const [amount, setAmount] = useState(0);
   
   return (
     <div>
@@ -19,6 +22,9 @@ export default function ListPage() {
             <div className="food" key={index}>
               <Image src={images[index]} alt={structure} className="food-img"></Image>
               <h4>{structure} is{costs[index]}$</h4>
+              <span> {amount} </span>
+              <button onClick={()=>{setAmount(amount+1)}}>+</button>
+              <button onClick={()=>{setAmount(amount-1)}}>-</button>
             </div>
           )
         })
